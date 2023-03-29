0545debc67a94f4b8c4ab8d9355d73ec4e48316a
krgarav <krgarav@gmail.com>
2023-03-30 00:07:58 +0530

    Enabling delete and add button in cart Items

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../Store/Cart-context";
+import CartItem from "./CartItem";
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const hasItems = cartCtx.items.length > 0;
+  const totalAmount = `Rs ${cartCtx.totalAmount.toFixed(2)}`;
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "m1", name: "sushi", amount: "2", price: "35.62" }].map(
-        (item) => {
-          return <li key={item.id}>{item.name}</li>;
-        }
-      )}
+      {cartCtx.items.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        );
+      })}
     </ul>
   );
 
@@ -17,13 +35,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button onClick={props.onClickClose} className={classes["btn--alt"]}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
